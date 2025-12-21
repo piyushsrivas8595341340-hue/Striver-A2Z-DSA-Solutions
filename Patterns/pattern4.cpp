f152f396c8b16d03ed14ebe7d6cdf9077bfdee47
Piyush Kumar <piyushsrivas8595341340@gmail.com>
2025-12-21 20:09:15 +0530

    Add files via upload
    This program displays a pattern which looks like :
    1
    22
    333
    4444
    55555

---
+++ Patterns/pattern4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void print(){
+    for (int i = 1; i < 6; i++)
+    {
+     for (int j = 1; j <= i; j++)
+     {
+        cout<<i;
+     }
+        cout<<endl;
+    }   
+}
+
+int main(){
+    print();
+
+    return 0;
+}
+/*
+
+1
+22
+333
+4444
+55555
+
+*/
