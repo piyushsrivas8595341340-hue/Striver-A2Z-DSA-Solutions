5d410d1fb4f2630b3cc7eed52ab9b781888829f6
Piyush Kumar <piyushsrivas8595341340@gmail.com>
2025-12-22 22:11:39 +0530

    Add files via upload
    This program displays a pattern which looks like :
    * 
    ** 
    ***
    ****
    *****
    ****
    ***
    **
    *

---
+++ Patterns/pattern10.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void print(){
+    for (int i = 1; i < 6; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"*";
+        }
+       cout<<endl; 
+    }
+    for (int i = 6; i < 10; i++)
+    {
+        for (int j = 1; j <= 10-i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    
+}
+
+int main(){
+    print();
+    return 0;
+}
+/*
+
+* 
+** 
+***
+****
+*****
+****
+***
+**
+*
+
+*/
