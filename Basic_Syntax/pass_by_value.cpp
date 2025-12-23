c3947c0c314d6ce347c461cb8b951444b4d33835
Piyush Kumar <piyushsrivas8595341340@gmail.com>
2025-12-23 17:11:42 +0530

    Add files via upload
    This program shows the working of a function called as pass by value.
    Pass by value: When a variable is passed by value, a copy of the variable is made. The function works on that copy, and the original variable remains unchanged.
    For example, Imagine passing my xeroxed marksheet copy to the company. If they stamp or mark it, my original marksheet remains untouched. That's pass by value.

---
+++ Basic_Syntax/pass_by_value.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void modify(int a){
+    a=a+15;
+}
+
+int main(){
+    int x=8;
+    modify(x);
+    cout<<"The value of x is : "<<x<<endl;
+
+    return 0;
+}
